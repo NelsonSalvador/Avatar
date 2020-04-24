25ebc087fa7b7c2751816859d0274d8ac7697496
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-04-24 11:14:10 +0100

    Set up test enviorment, Player movment script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{/*
+    [SerializeField] Transform followtarget;
+    [SerializeField] Vector3 offset;
+    [SerializeField] float feedBackLoopFactor = 0.1f;
+    [SerializeField] Rect cameraTrap;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 tragetPos = followtarget.position + offset;
+        tragetPos.z = transform.position.z;
+
+        Rect rect = GetWorldRect();
+
+        if (tragetPos.x < rect.xMin) rect.xMin = tragetPos.x;
+        else if (tragetPos.x > rect.xMin) rect.xMin - tragetPos.x;
+
+        if (tragetPos.y < rect.yMin) rect.yMin = tragetPos.y;
+        else if (tragetPos.y > rect.yMin) rect.yMin - tragetPos.y;
+
+
+        //Vector3 delta = tragetPos - transform.position;
+
+        //transform.position = transform.position + delta * feedBackLoopFactor;
+
+        transform.position - transform.position;
+    }
+    Rect GetWorldRect()
+    {
+        Rect rect = cameraTrap;
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0, 0.5f, 0.0f, 1.0f);
+
+        Vector3 p1 = new Vector3(cameraTrap.xMin + transform.position.x, cameraTrap.yMin + transform.position.y, 0);
+        Vector3 p2 = new Vector3(cameraTrap.xMax + transform.position.x, cameraTrap.yMin + transform.position.y, 0);
+        Vector3 p3 = new Vector3(cameraTrap.xMax + transform.position.x, cameraTrap.yMax + transform.position.y, 0);
+        Vector3 p4 = new Vector3(cameraTrap.xMin + transform.position.x, cameraTrap.yMax + transform.position.y, 0);
+
+
+       // Gizmos.
+    }*/
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float maxSpeed = 50.0f;
+    public float jumpSpeed = 300.0f;
+    public float jumpMaxTime = 0.1f;
+
+    public Transform groundCheck;
+    public LayerMask GroundLayers;
+
+    Rigidbody2D rb;
+
+    float jumpTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movimento em x
+        float hAxis = Input.GetAxis("Horizontal");
+
+        Vector2 currentVelocity = rb.velocity;
+
+        currentVelocity = new Vector2(maxSpeed * hAxis, currentVelocity.y);
+
+
+
+        Collider2D groundCollision = Physics2D.OverlapCircle(groundCheck.position, 5, GroundLayers);
+
+        bool oneGround = groundCollision != null;
+
+        // Salto
+        if ((Input.GetButtonDown("Jump")) && (oneGround))
+        {
+            currentVelocity.y = jumpSpeed;
+            rb.gravityScale = 0.0f;
+
+            jumpTime = Time.time;
+        }
+        else if ((Input.GetButtonDown("Jump")) && ((Time.time - jumpTime) < jumpMaxTime))
+        {
+            
+        }
+        else
+        {
+            rb.gravityScale = 5.0f;
+        }
+
+        rb.velocity = currentVelocity;
+
+    }
+}
