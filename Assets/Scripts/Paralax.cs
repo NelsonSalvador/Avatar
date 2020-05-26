46780ecfffd23b0d9b0bf8b4dd340e972f4bea02
NelsonSalvador <nelsonsalvadorzx@gmail.com>
2020-05-26 01:26:51 +0100

    Implemented paralax system

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    public float paralaxEfectMult = .5f;
+
+    private Transform cameraTransform;
+    private Vector3 lastCameraPos;
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPos = cameraTransform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 deltaMov = cameraTransform.position - lastCameraPos;
+
+
+        transform.position += deltaMov * paralaxEfectMult;
+        lastCameraPos = cameraTransform.position;
+    }
+}
